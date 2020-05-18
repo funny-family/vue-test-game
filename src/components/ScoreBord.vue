afac169e3b9b20c811de097dddccd06fa13075aa
funny family <maksvadik228@gmail.com>
2020-05-18 11:12:54 +0700

    completed main score counter functional

--- src/components/ScoreBord.vue
+++ src/components/ScoreBord.vue
@@ -3,27 +3,71 @@
     class="scoreboard-container"
     :style="{
       opacity:
-        isScoreboardVisible === true ? '1' :
+        show === true ? '1' :
         '0'
     }"
   >
-    <div class="current-score">
+    <div class="current-score score-style">
       Score: {{ currentScrore }}
+    </div>
+    <div class="first-place score-style">
+      Top 1: {{ modifiedScoreContainer[0] }}
+    </div>
+    <div class="second-place score-style">
+      Top 2: {{ modifiedScoreContainer[1] }}
+    </div>
+    <div class="third-place score-style">
+      Top 3: {{ modifiedScoreContainer[2] }}
     </div>
   </div>
 </template>
 
 <script>
+let scoreContainer = [].splice(0, 3).sort((a, b) => b - a);
+
 export default {
   props: {
     currentScrore: {
       type: Number,
       default: 0
     },
-    isScoreboardVisible: {
+    show: {
       type: Boolean,
-      default: false
+      default: true
     }
+  },
+  data: () => ({
+    firstPlace: 0,
+    secondPlace: 0,
+    thirdPlace: 0,
+    modifiedScoreContainer: [].splice(0, 3).sort((a, b) => b - a)
+  }),
+  // data() {
+  //   return {
+  //     firstPlace: 0,
+  //     secondPlace: 0,
+  //     thirdPlace: 0,
+  //     scoreContainer: [].splice(0,3) // set array limit to three elements
+  //   }
+  // },
+  computed: {
+    updatedScoreContainer() {
+      scoreContainer.push(this.$props.currentScrore);
+      let modifiedScoreContainer = this.$data.modifiedScoreContainer
+      modifiedScoreContainer = scoreContainer
+      return modifiedScoreContainer;
+
+      // scoreContainer.push(this.$props.currentScrore);
+      // scoreContainer.sort((a, b) => b - a);
+      // let modifiedScoreContainer = this.$data.modifiedScoreContainer;
+      // modifiedScoreContainer = scoreContainer;
+      // return modifiedScoreContainer;
+    }
+  },
+  mounted() {
+    // console.log(this.$data.modifiedScoreContainer);
+    console.log(this.updatedScoreContainer);
+    console.log(this.$data.modifiedScoreContainer);
   }
 }
 </script>
@@ -34,8 +78,24 @@ export default {
   padding-left: 60px;
   position: absolute;
 }
+
 .current-score {
   font-size: 28px;
+}
+
+.first-place {
+  font-size: 20px;
+}
+
+.second-place {
+  font-size: 20px;
+}
+
+.third-place {
+  font-size: 20px;
+}
+
+.score-style {
   font-weight: bolder;
 }
 </style>
